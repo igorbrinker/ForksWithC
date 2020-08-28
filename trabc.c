9fa43f0fa8e6fa0cb13d3e478c306c1abe498d88
Igor Brinker <igorbrinker@gmail.com>
2020-08-28 20:28:47 -0300

    Print Update

--- trabc.c
+++ trabc.c
@@ -40,8 +40,46 @@ void createEdge(fork *f, char source[], char destination[]){
         if(f->vertexes[i]->identifier == destination){
             vertexes_destination = f->vertexes[i];
         }
-        
     }
+    if(vertex_source == NULL || vertex_destination == NULL){
+        printf("ERRO: origem ou destino do vertice não encontrado(s)!\n");
+    } else{
+        int after_source = vertex_source->n;
+        vertex_source->edges[after_source] = vertex_destination;
+        vertex_source->n++;
 
+        int after_destination = vertex_destination->n;
+        vertex_destination->edges[after_destination] = vertex_source;
+        vertex destination->n++;
+    }
+}
+
+void print(fork *f){
+    printf("GRAFO DE VERTICES\n");
+    for(int i = 0; i < f->n; i++){
+        if(g->vertexes[i]->identifier != NULL){
+            printf("[%s]", f->vertexes[i]->identifier);
+            for(int i = 0; i < f->vertexes[i]->n; i++){
+                printf("-> (%s)", f->vertexes[i]->edges[i]->identifier);
+            }
+            printf("->[NULL]\n");
+        }
+    }
+}
+
+int main(){
+    Fork f;
+    initialize(&f);
+
+    insert(&f, "Ana");
+    insert(&f, "Pedro");
+    insert(&f, "Marta");
+    insert(&f, "Bento");
     
+    createEdge(&f, "Pedro", "Ana");
+    createEdge(&f, "Ana", "Marta");
+    createEdge(&f, "Bento", "Ana");
+
+    print(&f);
+    return 0;
 }
